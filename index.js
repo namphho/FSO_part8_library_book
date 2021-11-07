0b58468b5439fc57da0b4bbbe0699b09431827e1
Nam Pham <nam.phho@gmail.com>
2021-11-07 16:27:10 +0700

    8.5

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String!): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [AllAuthors!]!
   }
 `
@@ -115,7 +115,7 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      return books.filter((b) => b.author === args.author)
+      return books.filter((b) => b.genres.includes(args.genre))
     },
     allAuthors: () => {
       const groupOfAuthor = groupBy(books, (book) => book.author)
